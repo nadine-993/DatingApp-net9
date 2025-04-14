d0e4506d44ff604efc22b73febae0c295aaae4ec
nadine-993 <n.kantarji993@gmail.com>
2025-04-14 11:39:53 +0400

    End of Section 2

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using API.Data;
+using System.Net.Mime;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]// /api/users
+
+public class UserController(DataContext context): ControllerBase
+{
+
+
+
+[HttpGet]
+public async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+{
+var users = await context.Users.ToListAsync();
+
+return users;
+}
+
+
+[HttpGet("{id:int}")]// /api/users/3
+public  async Task<ActionResult<AppUser>>GetUser(int id)
+{
+var user = await context.Users.FindAsync(id);
+if (user == null) return NotFound();
+
+return user;
+}
+
+
+}
