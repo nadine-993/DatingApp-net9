f2bfb02caa3ec964cf61509ad8a8450822558c12
nadine-993 <n.kantarji993@gmail.com>
2025-08-07 16:28:22 +0400

    End of section 9

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,11 +6,12 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './__interceptors/error.interceptor';
+import { jwtInterceptor } from './__interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor]),),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor]),),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
